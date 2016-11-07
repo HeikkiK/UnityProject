b0a5926f667335cdaaaf2b816560a51200ced32f
HeikkiK <heikki.kortetmaa@hotmail.com>
2016-11-07 22:43:21 +0200

    Testing
    Player modified

--- GameProject/Assets/Scripts/PlayerController.cs
+++ GameProject/Assets/Scripts/PlayerController.cs
@@ -3,14 +3,18 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
+	public float Speed = 6.0F;
+	private Rigidbody rb2d;
+	void Start()
+	{
+		rb2d = GetComponent<Rigidbody> ();
+	}
 
-    void FixedUpdate()
-    {
+	void Update() 
+	{
 		float moveHorizontal = Input.GetAxis ("Horizontal");
-
-		float moveVertical = Input.GetAxis ("Vertical");
-
-        //float test = Input.GetAxis("Vertical");
-
-    }
+		float moveVertical = Input.GetAxis("Vertical");
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		rb2d.AddForce (movement * Speed);
+	}
 }

759edf8e5bd61bb64fa87936c2a9a2cf7ec1fa6b
HeikkiK <heikki.kortetmaa@hotmail.com>
2016-11-10 22:53:23 +0200

    Pieniä muutoksia siellä täällä
    Peli alkaa alusta jos törmää liian lujaa + pieniä muita juttuja

---
+++ GameProject/Assets/Scripts/SortingLayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SortingLayer : MonoBehaviour {
+
+	public string SortingLayerName = "UI";
+	public int SortingOrder = 0;
+
+	void Start ()
+	{
+		GetComponent<MeshRenderer> ().sortingLayerName = SortingLayerName;
+		GetComponent<MeshRenderer> ().sortingOrder = SortingOrder;
+		GetComponent<TextMesh> ().text = "Hei På Dej";
+	}
+}
